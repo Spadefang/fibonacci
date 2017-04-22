5824504f2ef70b211e5c8aab90d63e473ff38f06
Spadefang <spadefang@gmail.com>
2017-04-22 05:41:54 +0000

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+function fib(n) {
+    if(n === 0) {
+        return 0;
+    }else if(n === 1){
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2); //関数の定義内で関数自身を呼ぶ == 再帰
+}
+const length = 40;
+for (let i = 0; i <= length; i++){
+    console.log(fib(i));
+}
