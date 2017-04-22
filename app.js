c26a6ca6b362f0709c4a629bd2ab9228306c4051
Spadefang <spadefang@gmail.com>
2017-04-22 05:55:33 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 'use strict';
+const memo = new Map(); //memo = 新規の連想配列
+memo.set(0, 0); //memoの添字key0番目に0
+memo.set(1, 1); //memoの添字key1番目に1
 function fib(n) {
-    if(n === 0) {
-        return 0;
-    }else if(n === 1){
-        return 1;
+    if(memo.has(n)) { //添字keyがあったら
+        return memo.get(n); //添字key番目の値を返す（これで0番目0,1番目1が返却）
     }
-    return fib(n - 1) + fib(n - 2); //関数の定義内で関数自身を呼ぶ == 再帰
+    const value = fib(n - 1) + fib(n - 2); //関数の定義内で関数自身を呼ぶ == 再帰
+    memo.set(n, value); //添字key n番目にvalueをセット
+    return value;
 }
-const length = 40;
+const length = 100;
 for (let i = 0; i <= length; i++){
     console.log(fib(i));
 }
